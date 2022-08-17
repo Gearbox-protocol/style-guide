fefa75122df378aa15e1954a037b401af7aab21d
doomsower <K.Kyznetcov@gmail.com>
2022-08-17 12:44:53 +0300

    feat: relax typescript class rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,12 +4,14 @@ module.exports = {
     "alloy",
     "alloy/typescript",
     "plugin:import/recommended",
-    "plugin:import/typescript",
+    "plugin:import/typescript"
   ],
   env: {
-    node: true,
+    node: true
   },
   rules: {
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/member-ordering": "off",
     "@typescript-eslint/no-unused-vars": "off",
     "@typescript-eslint/no-floating-promises": "warn",
     "max-params": "off",
@@ -22,18 +24,18 @@ module.exports = {
         vars: "all",
         varsIgnorePattern: "^_",
         args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
+        argsIgnorePattern: "^_"
+      }
+    ]
   },
   settings: {
     "import/resolver": {
       typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-      },
-    },
+        alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      }
+    }
   },
   parserOptions: {
-    project: "./tsconfig.json",
-  },
+    project: "./tsconfig.json"
+  }
 };
